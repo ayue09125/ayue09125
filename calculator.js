1fd43a836305690195b37a443b3219a33e43a24c
ayue09125 <141705007+ayue09125@users.noreply.github.com>
2024-02-06 21:28:16 -0800

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -2,13 +2,17 @@ let question1Done = false;
 let question2Done = false;
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#crustacean').addEventListener('click', function(event) {
-        if (question1Done == false) {
-            document.querySelector('#crustacean').style.background = 'green';
-            document.getElementById("response").innerHTML = 'Correct!';
-            question1Done = true;
-            event.preventDefault();
-        }
+    document.querySelector('#calculate').addEventListener('submit', function(event) {
+        // if (question1Done == false) {
+        //     document.querySelector('#crustacean').style.background = 'green';
+        //     document.getElementById("response").innerHTML = 'Correct!';
+        //     question1Done = true;
+        //     event.preventDefault();
+        // }
+        let bw = document.querySelector('#bw').value;
+        let sf = document.querySelector('#sf').value;
+        let cr = document.querySelector('#cr').value;
+        let br = document.querySelector('#br').value;
     });
     document.querySelector('#arachnid').addEventListener('click', function(event) {
         if (question1Done == false) {
@@ -35,19 +39,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    document.querySelector('form').addEventListener('submit', function(event) {
-        if (question2Done == false) {
-            let answer = document.querySelector('#answer').value;
-            if (answer == '7' || answer.toLowerCase() == 'seven') {
-                document.getElementById("response2").innerHTML = 'Correct!';
-                document.querySelector('#answer').style.background = 'green';
-            }
-            else {
-                document.getElementById("response2").innerHTML = 'Incorrect';
-                document.querySelector('#answer').style.background = 'red';
-            }
-            question2Done = true;
-        }
-        event.preventDefault();
-    });
+    // document.querySelector('form').addEventListener('submit', function(event) {
+    //     if (question2Done == false) {
+    //         let answer = document.querySelector('#answer').value;
+    //         if (answer == '7' || answer.toLowerCase() == 'seven') {
+    //             document.getElementById("response2").innerHTML = 'Correct!';
+    //             document.querySelector('#answer').style.background = 'green';
+    //         }
+    //         else {
+    //             document.getElementById("response2").innerHTML = 'Incorrect';
+    //             document.querySelector('#answer').style.background = 'red';
+    //         }
+    //         question2Done = true;
+    //     }
+    //     event.preventDefault();
+    // });
 });
